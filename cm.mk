1d120b6f7801e28d70a01a5a202f4d70f9ad1360
Georgy Firsov <gfirsov007@gmail.com>
2015-04-22 16:22:43 +0300

    Update cm.mk
    LG-E975 -> Optimus G

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/lge/e975/e975.mk)
 PRODUCT_DEVICE := e975
 PRODUCT_NAME := cm_e975
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-E975
+PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
