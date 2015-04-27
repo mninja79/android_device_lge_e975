05e1ef0975c512fd16e1fdbdeb519054d75592a0
Daniel Sikorski <dadi.sikorski@gmail.com>
2015-04-27 22:20:31 +0200

    Update and rename cm.mk to slim.mk

--- slim.mk
+++ slim.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e975/e975.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e975
-PRODUCT_NAME := cm_e975
+PRODUCT_NAME := slim_e975
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
