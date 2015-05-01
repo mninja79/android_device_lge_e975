00b56d197c85ebf4aa2ebe986401897d098d4e4b
Daniel Sikorski <dadi.sikorski@gmail.com>
2015-05-01 22:38:19 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -97,7 +97,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += \
 	hostapd_default.conf \
-	wpa_supplicant.conf \
 	wpa_supplicant_overlay.conf \
 	p2p_supplicant.conf \
 	p2p_supplicant_overlay.conf \
